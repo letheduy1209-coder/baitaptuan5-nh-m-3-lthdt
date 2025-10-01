a9f39ed1ed006b8a3b816cd8e21691a75c40f6ad
Ngyuen Bao Phuc <phucnguyen2k20@gmail.com>
2025-10-01 22:39:17 +0700

    Push Sach.java and Test.java

---
+++ nhom3/src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+    public static void main(String[] args) {
+        Sach laptrinh = new Sach("B001", "Lập trình Java", "Nguyễn Văn A", 2021, 10);
+        Sach ctdlgt = new Sach();
+        ctdlgt.setMaSach("B002");
+        ctdlgt.setTieuDe("Cấu trúc dữ liệu");
+        ctdlgt.setTacGia("Trần Thị B");
+        ctdlgt.setNamXuatBan(2020);
+        ctdlgt.setSoLuong(5);
+
+        laptrinh.hienThiThongTin();
+        ctdlgt.hienThiThongTin();
+    }
+}
+
